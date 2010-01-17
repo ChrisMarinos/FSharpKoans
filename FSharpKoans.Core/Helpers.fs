470fc2c0d62b64855ac58b239e658026ade17f3b
Chris Marinos <cmmarinos@gmail.com>
2010-01-16 23:40:33 -0500

    Missed a file

---
+++ FSharpKoans.Core/Helpers.fs
@@ -0,0 +1,16 @@
+﻿[<AutoOpenAttribute>]
+module FSharpKoans.Core.Helpers
+
+open NUnit.Framework
+
+let __ = "FILL ME IN"
+
+let (=) x y =
+    x = y.ToString()
+
+let AssertWithMessage x message = Assert.IsTrue(x, message)
+
+let AssertEquality (x:'a) (y:'b) = Assert.AreEqual(x,y)   
+
+let Assert x = Assert.IsTrue(x)
+
