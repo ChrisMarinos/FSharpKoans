69771fafd7f755d3f32219efad47c09bf866fd17
Chris Marinos <cmmarinos@gmail.com>
2010-02-24 00:22:29 -0500

    Added koans for tuples.

---
+++ FSharpKoans/AboutTuples.fs
@@ -0,0 +1,63 @@
+﻿namespace FSharpKoans
+open FSharpKoans.Core
+
+type ``about tuples``() =
+    
+    [<Koan>]
+    member this.CreatingTuples() =
+        let items = ("apple", "dog")
+        
+        AssertEquality items __
+        
+    [<Koan>]
+    member this.AccessingTupleElements() =
+        let items = ("apple", "dog")
+        
+        let fruit = fst items
+        let animal = snd items
+        
+        AssertEquality fruit __
+        AssertEquality animal __
+        
+    [<Koan>]
+    member this.AccessingTupleElementsWithPatternMatching() =
+        let items = ("apple", "dog", "Mustang")
+        
+        let fruit, animal, car = items
+        
+        AssertEquality fruit __
+        AssertEquality animal __
+        AssertEquality car __
+        
+    [<Koan>]
+    member this.IgnoringValuesWithPatternMatching() =
+        let items = ("apple", "dog", "Mustang")
+        
+        let _, animal, _ = items
+        
+        AssertEquality animal __
+    
+    (* NOTE: Pattern matching is found in many places
+             throughout F#, we'll revisit it again later *)
+        
+    [<Koan>]
+    member this.ReturningMultipleValuesFromAFunction() =
+        let squareAndCube x =
+            (x ** 2.0, x ** 3.0)
+        
+        let squared, cubed = squareAndCube 3.0
+        
+        
+        AssertEquality squared __
+        AssertEquality cubed __
+    
+    //THINK ABOUT IT: Are there really multiple return values?
+    
+    [<Koan>]
+    member this.TheTruthBehindMultipleReturnValues() =
+        let squareAndCube x =
+            (x ** 2.0, x ** 3.0)
+            
+        let result = squareAndCube 3.0
+       
+        AssertEquality result __
