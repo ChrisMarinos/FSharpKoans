455d26391310502128bcd20cdf27ac4ce55dc77d
Chris Marinos <cmmarinos@gmail.com>
2011-11-01 06:11:18 -0400

    added koan for partial application

--- FSharpKoans/MoreAboutFunctions.fs
+++ FSharpKoans/MoreAboutFunctions.fs
@@ -17,3 +17,22 @@ type ``more about functions``() =
         (* The fun keyword allows you to create a function inline without giving
            it a name. These functions are known as anonymous functions, lambdas,
            or lambda functions. *)
+
+    [<Koan>]
+    member this.PartialApplication() =
+        let add x y = 
+            x + y
+
+        (* You already know that you can pass multiple arguments to a function 
+           at once *)
+        let thirteen = add 6 7
+        AssertEquality thirteen __
+
+        (* but you can also pass arguments one at a time to create residual 
+          functions. This is called Partial Application. *)
+        let addSeven = add 7
+        let unluckyNumber = addSeven 6
+        let luckyNumber = addSeven 0
+
+        AssertEquality unluckyNumber __
+        AssertEquality luckyNumber __
