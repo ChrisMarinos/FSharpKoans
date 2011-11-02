ef70c9cd6b4bfc950d9b31827e4556d1f38ae8f2
Chris Marinos <cmmarinos@gmail.com>
2011-11-01 22:13:48 -0400

    added some extra koans for option types

--- FSharpKoans/AboutOptionTypes.fs
+++ FSharpKoans/AboutOptionTypes.fs
@@ -1,6 +1,12 @@
 ﻿namespace FSharpKoans
 open FSharpKoans.Core
 
+type Game = {
+    Name: string
+    Platform: string
+    Score: int option
+}
+
 type ``about option types``() =
 
     [<Koan>]
@@ -18,3 +24,39 @@ type ``about option types``() =
         AssertEquality noValue.IsSome __
         AssertEquality noValue.IsNone __
         AssertThrows<FILL_IN_THE_EXCEPTION> (fun () -> noValue.Value __)
+
+    [<Koan>]
+    member this.UsingOptionTypesWithPatternPatching() =
+        let chronoTrigger = { Name = "Chrono Trigger"; Platform = "SNES"; Score = Some 5 }
+        let gta = { Name = "Halo"; Platform = "Xbox"; Score = None }
+
+        let translate score =
+            match score with
+            | 5 -> "Great"
+            | 4 -> "Good"
+            | 3 -> "Decent"
+            | 2 -> "Bad"
+            | 1 -> "Awful"
+            | _ -> "Unknown"
+
+        let getScore game =
+            match game.Score with
+            | Some score -> translate score
+            | None -> "Unknown"
+
+        AssertEquality (getScore chronoTrigger) __
+        AssertEquality (getScore gta) __
+
+    [<Koan>]
+    member this.ProjectingValuesFromOptionTypes() =
+        let chronoTrigger = { Name = "Chrono Trigger"; Platform = "SNES"; Score = Some 5 }
+        let gta = { Name = "Halo"; Platform = "Xbox"; Score = None }
+
+        let decideOn game =
+
+            game.Score
+            |> Option.map (fun score -> if score > 3 then "play it" else "don't play")
+
+        //HINT: look at the return type of the decide on function
+        AssertEquality (decideOn chronoTrigger) __
+        AssertEquality (decideOn chronoTrigger) __
