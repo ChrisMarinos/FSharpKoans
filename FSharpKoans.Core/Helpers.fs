fdc65956f01666c04c5157c032780c85c1d4eba4
Chris Marinos <cmmarinos@gmail.com>
2011-11-01 20:19:58 -0400

    added some kaons for option types

--- FSharpKoans.Core/Helpers.fs
+++ FSharpKoans.Core/Helpers.fs
@@ -1,5 +1,6 @@
 ﻿[<AutoOpenAttribute>]
 module FSharpKoans.Core.Helpers
+open System
 open System.CodeDom
 open NUnit.Framework
 
@@ -8,10 +9,15 @@ let __ = "FILL ME IN"
 type FILL_ME_IN =
     class end
 
+type FILL_IN_THE_EXCEPTION() =
+    inherit Exception()
+
 let AssertWithMessage x message = Assert.IsTrue(x, message)
 
 let AssertEquality (x:'a) (y:'b) = Assert.AreEqual(x,y)   
 
 let AssertInequality (x:'a) (y:'b) = Assert.AreNotEqual(x,y)
+
+let AssertThrows<'a when 'a :> exn> action = Assert.Throws<'a>(fun () -> action())
 
 let Assert x = Assert.IsTrue(x)
---
+++ FSharpKoans/AboutOptionTypes.fs
@@ -0,0 +1,20 @@
+﻿namespace FSharpKoans
+open FSharpKoans.Core
+
+type ``about option types``() =
+
+    [<Koan>]
+    member this.OptionTypesMightContainAValue() =
+        let someValue = Some 10
+        
+        AssertEquality someValue.IsSome __
+        AssertEquality someValue.IsNone __
+        AssertEquality someValue.Value __
+
+    [<Koan>]
+    member this.OrTheyMightNot() =
+        let noValue = None
+
+        AssertEquality noValue.IsSome __
+        AssertEquality noValue.IsNone __
+        AssertThrows<FILL_IN_THE_EXCEPTION> (fun () -> noValue.Value __)
