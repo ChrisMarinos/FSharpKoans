11652e8bdd5117374848bbd094950f1bdf5909c0
Chris Marinos <cmmarinos@gmail.com>
2011-11-03 07:58:31 -0400

    added koans for arrays and .net collections

---
+++ FSharpKoans/AboutArrays.fs
@@ -0,0 +1,48 @@
+﻿namespace FSharpKoans
+open FSharpKoans.Core
+open System.Collections.Generic
+
+type ``about arrays``() =
+    [<Koan>]
+    member this.CreatingArrays() =
+        let fruits = [| "apple"; "pear"; "peach"|]
+
+        AssertEquality fruits.[0] __
+        AssertEquality fruits.[1] __
+        AssertEquality fruits.[2] __
+
+    [<Koan>]
+    member this.ArraysAreDotNetArrays() =
+        let fruits = [| "apple"; "pear" |]
+
+        let arrayType = fruits.GetType()
+        let systemArray = System.Array.CreateInstance(typeof<string>, 0).GetType()
+
+        (* Unlike List, Arrays in F# are the standard .NET arrays that
+           you're used to if you're coming from another .NET language *)
+        AssertEquality arrayType systemArray
+
+    [<Koan>]
+    member this.ArraysAreMutable() =
+        let fruits = [| "apple"; "pear" |]
+        fruits.[1] <- "peach"
+
+        AssertEquality fruits __
+
+    [<Koan>]
+    member this.YouCanCreateArraysWithComprehensions() =
+        let numbers = 
+            [| for i in 0..10 do 
+                   if i % 2 = 0 then yield i |]
+
+        AssertEquality numbers __
+
+    [<Koan>]
+    member this.ThereAreAlsoSomeOperationsYouCanPerformOnArrays() =
+        let cube x =
+            x * x * x
+
+        let original = [| 0..5 |]
+        let result = Array.map cube original
+
+        AssertEquality original __
---
+++ FSharpKoans/AboutDotNetCollections.fs
@@ -0,0 +1,40 @@
+﻿namespace FSharpKoans
+open FSharpKoans.Core
+open System.Collections.Generic
+
+type ``about dot net collections``() =
+
+    [<Koan>]
+    member this.CreatingDotNetLists() =
+        let fruits = new List<string>()
+
+        fruits.Add("apple")
+        fruits.Add("pear")
+ 
+        AssertEquality fruits.[0] __
+        AssertEquality fruits.[1] __
+
+    [<Koan>]
+    member this.CreatingDotNetDictionaries() =
+        let addressBook = new Dictionary<string, string>()
+
+        addressBook.["Chris"] <- "Ann Arbor"
+        addressBook.["SkillsMatter"] <- "London"
+
+        AssertEquality addressBook.["Chris"] __
+        AssertEquality addressBook.["SkillsMatter"] __
+
+    [<Koan>]
+    member this.YouUseCombinatorsWithDotNetTypes() =
+        let addressBook = new Dictionary<string, string>()
+
+        addressBook.["Chris"] <- "Ann Arbor"
+        addressBook.["SkillsMatter"] <- "London"
+
+        let verboseBook = 
+            addressBook
+            |> Seq.map (fun kvp -> sprintf "Name: %s - Address: %s" kvp.Key kvp.Value)
+            |> Seq.toArray
+
+        AssertEquality verboseBook.[0] __
+        AssertEquality verboseBook.[1] __
