fb14cfe7bd71617a81c92e9aa0f79caccffd3f8d
Chris Marinos <cmmarinos@gmail.com>
2011-11-02 23:33:35 -0400

    added koans for unit

---
+++ FSharpKoans/AboutUnit.fs
@@ -0,0 +1,22 @@
+﻿namespace FSharpKoans
+open FSharpKoans.Core
+open Microsoft.FSharp.Reflection
+
+type ``about unit``() =
+
+    [<Koan>]
+    member this.UnitIsUsedWhenThereIsNoReturnValueForAFunction() =
+        let sendData data =
+            //send the data to the server
+            ()
+
+        let x = sendData "data"
+        AssertEquality x __ //Don't overthink this
+
+    [<Koan>]
+    member this.ParameterlessFuncitonsTakeUnitAsTheirArgument() =
+        let sayHello() =
+            "hello"
+
+        let result = sayHello()
+        AssertEquality result __
