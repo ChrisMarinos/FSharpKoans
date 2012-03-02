545e38fc6b82f2115284f385000d80e1ea7f9eb3
Chris Marinos <cmmarinos@gmail.com>
2012-03-01 21:00:12 -0500

    added a map function for KoanResult

--- FSharpKoans.Core/KoanResult.fs
+++ FSharpKoans.Core/KoanResult.fs
@@ -10,3 +10,10 @@ with
         match this with
         | Success x -> x
         | Failure (x, _) -> x
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module KoanResult =
+    let map f (x:KoanResult) =
+        match x with
+        | Success m -> Success <| f m
+        | Failure (m, e) -> Failure (f m, e)
---
+++ FSharpKoans.Test/KoanResults.fs
@@ -0,0 +1,24 @@
+﻿module KoansRunner.Test.KoanResults
+open FSharpKoans.Core
+open NUnit.Framework
+
+[<Test>]
+let ``map lets you project a message when success``() =
+    let result = Success "sample message"
+
+    let mappedResult = 
+        result
+        |> KoanResult.map (fun x -> x + " expanded")
+
+    Assert.AreEqual(Success "sample message expanded", mappedResult)
+
+[<Test>]
+let ``map lets you project a message when failure``() =
+    let ex = new System.Exception("abcd")
+    let result = Failure ("sample message", ex)
+
+    let mappedResult = 
+        result
+        |> KoanResult.map (fun x -> x + " expanded")
+
+    Assert.AreEqual(Failure ("sample message expanded", ex), mappedResult)
