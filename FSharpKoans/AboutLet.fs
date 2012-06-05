d7e0541c7a2f23a7f1f0e7addaf31695f9bf5878
Chris Marinos <cmmarinos@gmail.com>
2012-06-05 10:10:52 -0400

    added a koan about the difference between int and double... may need to come back to this later

--- FSharpKoans/AboutLet.fs
+++ FSharpKoans/AboutLet.fs
@@ -45,6 +45,24 @@ type ``about let``() =
            contexts as you'll see later. *)
     
     [<Koan>]
+    member this.FloatsAndInts() =
+        (* Depending on your background, you may be surprised to learn that
+           in F#, integers and floating point numbers are different types. 
+           In other words, the following is true. *)
+        let x = 20
+        let typeOfX = x.GetType()
+
+        let y = 20.0
+        let typeOfY = y.GetType()
+
+        //you don't need to modify these
+        AssertEquality typeOfX typeof<int>
+        AssertEquality typeOfY typeof<float>
+
+        //If you're coming from another .NET language, float is F# slang for
+        //the double type.
+
+    [<Koan>]
     member this.ModifyingTheValueOfVariables() =
         let mutable x = 100
         x <- 200
