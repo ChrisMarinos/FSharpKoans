a30e5cbc35b383186a501fa7c54791bf52688c7a
askingalot <askingalot@gmail.com>
2013-06-25 19:10:35 -0500

    Wrapping Tuples to correct .ToString() issue.
    NUnit's Assert.AreEqual() method was displaying a this (9.0, 27.0) as (9, 27).  This fix introduces an intermediate object that overrides ToString() using sprintf

--- FSharpKoans.Core/Helpers.fs
+++ FSharpKoans.Core/Helpers.fs
@@ -12,11 +12,30 @@ type FILL_ME_IN =
 type FILL_IN_THE_EXCEPTION() =
     inherit Exception()
 
+type TupleWrapper(innerTuple) =
+    member this.InnerTuple = innerTuple
+    override this.ToString() = sprintf "%A" innerTuple
+    override this.Equals that =
+        match that with
+        | :? TupleWrapper -> this.InnerTuple = (that :?> TupleWrapper).InnerTuple
+        | _ -> false
+
+let prepareAssertParam (param:obj) =
+    if param <> null && FSharpType.IsTuple (param.GetType()) then
+        new TupleWrapper(param) :> obj
+    else 
+        param
+
+let AssertEquality (x:'a) (y:'b) = 
+    Assert.AreEqual( prepareAssertParam x, 
+                     prepareAssertParam y)   
+
+let AssertInequality (x:'a) (y:'b) = 
+    Assert.AreNotEqual( prepareAssertParam x,
+                        prepareAssertParam y)
+
 let AssertWithMessage x message = Assert.IsTrue(x, message)
 
-let AssertEquality (x:'a) (y:'b) = Assert.AreEqual(x,y)   
-
-let AssertInequality (x:'a) (y:'b) = Assert.AreNotEqual(x,y)
 
 let AssertThrows<'a when 'a :> exn> action = Assert.Throws<'a>(fun () -> action())
 
