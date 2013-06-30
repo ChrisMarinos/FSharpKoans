4221c22cef13c73917128b442866a064f2a5d773
Jakub Fojtl <jfojtl@gmail.com>
2013-06-30 17:27:52 +0200

    fixed 5

--- FSharpKoans/AboutUnit.fs
+++ FSharpKoans/AboutUnit.fs
@@ -19,7 +19,7 @@ module ``about unit`` =
             ()
 
         let x = sendData "data"
-        AssertEquality x __ //Don't overthink this
+        AssertEquality x () //Don't overthink this
 
     [<Koan>]
     let ParameterlessFunctionsTakeUnitAsTheirArgument() =
@@ -27,4 +27,4 @@ module ``about unit`` =
             "hello"
 
         let result = sayHello()
-        AssertEquality result __
+        AssertEquality result "hello"
