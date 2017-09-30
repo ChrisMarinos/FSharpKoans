4186d58ea9e3be38a7cb1aed69b602018b6e9953
Kirill Bulatov <mail4score@gmail.com>
2017-09-30 23:46:11 +0300

    Use .NET Core 2.0 to build the project

--- FSharpKoans.Core/Helpers.fs
+++ FSharpKoans.Core/Helpers.fs
@@ -12,12 +12,12 @@ type FILL_ME_IN =
 type FILL_IN_THE_EXCEPTION() =
     inherit Exception()
 
-let AssertWithMessage x message = Assert.IsTrue(x, message)
+let AssertWithMessage (x : bool) message = Assert.IsTrue(x, message)
 
-let AssertEquality (x:'T) (y:'T) = Assert.AreEqual(x,y)   
+let AssertEquality (x:'T) (y:'T) = Assert.AreEqual(x,y)
 
 let AssertInequality (x:'T) (y:'T) = Assert.AreNotEqual(x,y)
 
 let AssertThrows<'a when 'a :> exn> action = Assert.Throws<'a>(fun () -> action())
 
-let Assert x = Assert.IsTrue(x)
+let Assert (x : bool) = Assert.IsTrue(x)
