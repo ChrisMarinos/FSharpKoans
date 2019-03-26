400a467c462aa18eba449909d2de3f0a987d61c5
YM <y.motara@ru.ac.za>
2019-03-26 10:17:28 -0700

    Typed comparison for Result<'a,'b>

--- FSharpKoans/AboutOptions.fs
+++ FSharpKoans/AboutOptions.fs
@@ -1,5 +1,6 @@
 ﻿namespace FSharpKoans
 open NUnit.Framework
+open FsUnitTyped
 
 (*
 In F#, we tend to use options instead of exceptions for error-handling.
@@ -93,6 +94,6 @@ module ``11: Exploring types, options, and results`` =
             | _ ->
                 // 'sqrt' is the square-root function
                 __ (sqrt n / m)
-        f -6.0 2.5 |> should equal __
-        f 144.0 2.0 |> should equal (Ok 6.0)
-        f 7.3 0.0 |> should equal (Error DivisionByZero)
+        f -6.0 2.5 |> shouldEqual __
+        f 144.0 2.0 |> shouldEqual (Ok 6.0)
+        f 7.3 0.0 |> shouldEqual (Error DivisionByZero)
